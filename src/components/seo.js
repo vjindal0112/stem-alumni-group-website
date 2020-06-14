92a0dfa69f9e4f61671a62666a2d037f5fc1c8e0
Varun Jindal <vjindal0112@gmail.com>
2020-06-14 01:21:22 -0400

    try another thing for og image

--- src/components/seo.js
+++ src/components/seo.js
@@ -53,7 +53,7 @@ function SEO({ description, lang, meta, title }) {
         },
         {
           property: `og:image`,
-          content: "STEMAlumniLogoFinal.png",
+          content: "./STEMAlumniLogoFinal.png",
         },
         {
           property: `og:description`,
